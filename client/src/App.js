4689cf8e20c8dd18e9f4b4b56ca62ef218ddacd9
Parassahu2308 <sahujuparas786@gmail.com>
2023-02-03 11:22:22 +0530

    :lipstick: some changes in ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Home from "./pages/Home";
@@ -7,7 +7,34 @@ import Login from "./pages/Login";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
-  const user = false;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:5000/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("Authentication gas been failed!");
+        })
+        .then((responseObj) => {
+          setUser(responseObj.user);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    };
+    getUser();
+  }, []);
+
+  console.log(user);
 
   return (
     <BrowserRouter>
